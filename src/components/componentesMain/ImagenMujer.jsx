b939968d5bc3c90a649270896cdd555cc4c0dfac
PaniSoto <mpanisoto@gmail.com>
2025-02-23 16:14:20 +0100

    imagenesInicio

--- src/components/componentesMain/ImagenMujer.jsx
+++ src/components/componentesMain/ImagenMujer.jsx
@@ -3,7 +3,7 @@ function ImagenMujer() {
         <a href="/mujeres" className="group">
             <img
                 className="w-[850px] h-[737px] object-cover object-top"
-                src="../src/assets/imagenes/Mujer.jpg"
+                src="./src/assets/imagenes/Mujer.jpg"
                 alt="Enlace a la ropa de mujer"
             />
             <h1 className="absolute right-70 top-85 transform -translate-y-1/2 text-white text-3xl transition-transform duration-300 scale-0 group-hover:scale-110 underline">
