3a441df61c8953af7ff5240552a3526ffc6e0aa9
PaniSoto <mpanisoto@gmail.com>
2025-02-23 16:28:34 +0100

    commit con Link

--- src/components/componentesMain/ImagenHombre.jsx
+++ src/components/componentesMain/ImagenHombre.jsx
@@ -1,16 +1,18 @@
+import { Link } from "react-router-dom";
+
 function ImagenHombre() {
     return (
 
-        <a href="/hombres" className="group">
+        <Link href="/hombres" className="group">
             <img
                 className="w-[850px] h-[737px] object-cover object-top"
-                src="./src/assets/imagenes/Hombre.jpg"
+                src="../src/assets/imagenes/Hombre.jpg"
                 alt="Enlace a la ropa de hombre"
             />
             <h1 className="absolute left-70 top-85 transform -translate-y-1/2 text-white text-3xl transition-transform duration-300 scale-0 group-hover:scale-110 underline">
                 Hombres
             </h1>
-        </a>
+        </Link>
     );
 }
 
--- src/components/componentesMain/ImagenMujer.jsx
+++ src/components/componentesMain/ImagenMujer.jsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
+
 function ImagenMujer() {
     return (
-        <a href="/mujeres" className="group">
+        <Link href="/mujeres" className="group">
             <img
                 className="w-[850px] h-[737px] object-cover object-top"
                 src="./src/assets/imagenes/Mujer.jpg"
@@ -9,7 +11,7 @@ function ImagenMujer() {
             <h1 className="absolute right-70 top-85 transform -translate-y-1/2 text-white text-3xl transition-transform duration-300 scale-0 group-hover:scale-110 underline">
                 Mujeres
             </h1>
-        </a>
+        </Link>
     );
 }
 
